531ad6db89cb097bbe99a2e2073f0669df5da908
LYJ <532874387@qq.com>
2025-08-23 20:37:57 +0800

    更新

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("plugins.plugins-setup") -- lazy 插件管理
+
+require("core.options") -- Nvim 配置
+require("core.keymap")-- 键位映射
+
+require("plugins.lualine") -- 状态栏
+require("plugins.mini-files") -- 状态栏
+require("plugins.mini-icons") -- 状态栏
+require("plugins.treesitter") -- 语法高亮
---
+++ lua/core/keymap.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " " --导航健
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "jk", "<ESC>") -- jk退出
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- mini 文件浏览
+keymap.set("n", "<leader>e", ":lua MiniFiles.open()<CR>")
+
+
